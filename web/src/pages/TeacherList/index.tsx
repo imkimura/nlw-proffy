2067c541bc93701d66af4e39ab460bb29931f6b8
jkssilva <juliakimura@univem.edu.br>
2020-08-05 14:17:32 -0300

    started 3 day of nlw

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,26 +3,45 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
                 <form action="" id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
                     
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Física', label: 'Física' },
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Select 
+                        name="week_day" 
+                        label="Dia da Semana" 
+                        options={[
+                            { value: '0', label: 'Segunda' },
+                            { value: '1', label: 'Terça' },
+                            { value: '2', label: 'Quarta' },
+                            { value: '3', label: 'Quinta' },
+                            { value: '4', label: 'Sexta' },
+                            { value: '5', label: 'Sábado' },
+                            { value: '6', label: 'Domingo' },
+                        ]}
+                    />
+                    
+                    <Input type="time" name="time" label="Hora" />
+
                 </form>
             </PageHeader>
 
